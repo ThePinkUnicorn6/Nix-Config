7b025aafda6a7151c2051b44fe0d70cc1a275de7
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2024-03-22 14:52:54 +0000

    change gammastep day value

--- user/app/desktop/gammastep/default.nix
+++ user/app/desktop/gammastep/default.nix
@@ -5,6 +5,7 @@
     enable = true;
     tray = true;
     temperature.night = 3500;
+    temperature.day = 6500;
     latitude = settings.user.location.lat;
     longitude = settings.user.location.lon;
   };
