26b4483d4a5991be491feb0c4913318f429a64cf
ThePinkUnicorn <theotopham@gmail.com>
2024-04-17 21:44:19 +0100

    Rework update script to wrap nh

--- user/app/shell/scripts/update.nix
+++ user/app/shell/scripts/update.nix
@@ -2,8 +2,9 @@
 
 {
   home.packages = with pkgs; [
+
     # Scripts
-    (pkgs.writeScriptBin "update" (''
+    (pkgs.writeScriptBin "nu" (''
       #!/usr/bin/env bash
 
       set -e
@@ -13,9 +14,6 @@
         "install")
           echo "Not implemented yet...";;
 
-        "log")
-          cat "update.log";;
-
         "history")
           git log -p $2;;
 
@@ -24,34 +22,27 @@
           git push origin main;;
 
         "test")
-          git --no-pager diff -U0
-          echo "Building test config..."
           # Add all files to commit so the flake can see them.
           git add -A
 
           # Build test system. Will use home manager if on another os, and nixos-rebuild if not.
 
-          sudo '' + (if (settings.system.isNixOS) then ''nixos-rebuild test --flake .#system'' else ''home-manager switch --flake .#user'') + '' | tee update.log
-          echo "NixOS Test Built OK!";;
+          ${nh}/bin/nh os test . -H system;;
 
         "flake")
           echo "Updating flake lock file..."
           nix flake update .;;
 
         *)
-          git reset > /dev/null # Uncommit everything so it shows in the diff, it will be added back in a couple of lines anyway.
-          git --no-pager diff -U0
           git add -A
-          echo "Building config and committing..."
           read -rp "Enter commit message (leave blank for generation number): " msg
-          sudo '' + (if (settings.system.isNixOS) then ''nixos-rebuild switch --flake .#system'' else ''home-manager switch --flake .#user'') + '' | tee update.log
+          ${nh}/bin/nh os switch . -a -H system
 
           # If the user has entered no comit message, generate it.
           if ! [[ -n "$msg" ]]; then
             msg=$(nixos-rebuild list-generations | grep current)
           fi
           git commit -am "$msg"
-          echo "NixOS Rebuilt OK!";;
       esac
 
       popd > /dev/null
