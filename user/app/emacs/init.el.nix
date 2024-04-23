585a502eb29d04f002db2248085d58f342e79ee4
ThePinkUnicorn <theotopham@gmail.com>
2024-04-23 22:20:25 +0100

    Update flake

--- user/app/emacs/init.el.nix
+++ user/app/emacs/init.el.nix
@@ -189,7 +189,7 @@ home.file.".config/doom/init.el".text = ''
        :app
        ;;calendar
        ;;emms
-       ;;everywhere        ; *leave* Emacs!? You must be joking
+       everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
