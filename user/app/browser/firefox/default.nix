5292e01503598ea58b006c9bcb29edc1bcfee646
ThePinkUnicorn <theotopham@gmail.com>
2024-04-29 21:04:59 +0100

    Add option to run any package from the nix repository by name

--- user/app/browser/firefox/default.nix
+++ user/app/browser/firefox/default.nix
@@ -5,7 +5,30 @@
   programs.firefox = {
     enable = true;
     profiles.yo7bc8tq = {
-      extraConfig = (builtins.readFile ./FF-ULTIMA/user.js);
+      settings = {
+        "ultima.tabs.vertical" = false;
+        "ultima.tabs.xs" = false;
+        "ultima.tabs.s" = false;
+        "ultima.tabs.l" = true;
+        "ultima.tabs.autohide" = true;
+
+        "ultima.sidebar.autohide" = true;
+
+        "ultima.xstyle.squared" = false;
+        "ultima.xstyle.containertabs.i" = false;
+        "ultima.xstyle.containertabs.ii" = false;
+        "ultima.xstyle.containertabs.iii" = true;
+        "ultima.theme.extensions" = true;
+
+        "ultima.OS.kde" = true;
+        "ultima.OS.gnome" = false;
+        "ultima.OS.mac" = false;
+
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "svg.context-properties.content.enabled" = true;
+        "toolkit.tabbox.switchByScrolling" = false;
+      };
+      # extraConfig = (builtins.readFile ./FF-ULTIMA/user.js);
       extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
         ublock-origin
         sidebery
