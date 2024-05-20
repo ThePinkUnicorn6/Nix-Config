ba8449aea7ad35d4149ff665d25475a0050d53c9
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2024-05-20 16:27:47 +0100

    Remove images from ffultima

--- settings.nix
+++ settings.nix
@@ -12,7 +12,6 @@
     email = "***REMOVED******REMOVED***"; # Your email (used for git).
     gitName = "ThePinkUnicorn";
     theme = "gruvbox-dark-soft"; # The name of the theme, as in https://tinted-theming.github.io/base16-gallery/.
-    themePolarity = "";
     wallpaper = /home/***REMOVED***/nixos/themes/wallpapers/city_sunset.png;
     reThemeWall = true; # Re-theme the colours of the paper with imagemagic
     wm = "hyprland";
