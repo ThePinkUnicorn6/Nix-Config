44fc972a0f7ce68def6f138aba366ed070c679d9
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2024-08-14 09:56:40 +0100

    Try xfce again

--- modules/nix/wm/xfce-i3.nix
+++ modules/nix/wm/xfce-i3.nix
@@ -4,13 +4,9 @@
   services.xserver = {
     enable = true;
     desktopManager = {
-      xterm.enable = false;
       xfce = {
         enable = true;
-        noDesktop = true;
-        enableXfwm = false;
       };
     };
-    windowManager.i3.enable = true;
   };
 }
