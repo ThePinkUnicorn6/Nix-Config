3c6d2e21d32e0cb65c38de3bac47aa98ff6ed1c9
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2024-10-19 00:12:28 +0100

    15 current  2024-10-19 00:12:27  24.11.20241014.a3c0b3b   6.10.5                          *

--- modules/hm/wm/hyprland/default.nix
+++ modules/hm/wm/hyprland/default.nix
@@ -52,192 +52,192 @@ in{
     ] else [];
     package = if isLaptop then inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland else pkgs.hyprland;
     settings = {
-        monitor = if isDesktop then [
-            "HDMI-A-1,preferred,0x770,1"
-            "DP-2,preferred,auto-left,1"
-            "DP-2,transform,1"
-        ] else [];
+      monitor = if isDesktop then [
+        "HDMI-A-1,preferred,0x770,1"
+        "DP-2,preferred,auto-left,1"
+        "DP-2,transform,1"
+      ] else [];
 
-        # Workspace monitor binding
-        workspace =  if isDesktop then [
-            "1,monitor:HDMI-A-1"
-            "2,monitor:HDMI-A-1"
-            "3,monitor:HDMI-A-1"
-            "4,monitor:HDMI-A-1"
-            "5,monitor:HDMI-A-1"
-            "6,monitor:DP-2"
-            "7,monitor:DP-2"
-            "8,monitor:DP-2"
-            "9,monitor:DP-2"
-            "10,monitor:DP-2"
-        ] else [];
+      # Workspace monitor binding
+      workspace =  if isDesktop then [
+        "1,monitor:HDMI-A-1"
+        "2,monitor:HDMI-A-1"
+        "3,monitor:HDMI-A-1"
+        "4,monitor:HDMI-A-1"
+        "5,monitor:HDMI-A-1"
+        "6,monitor:DP-2"
+        "7,monitor:DP-2"
+        "8,monitor:DP-2"
+        "9,monitor:DP-2"
+        "10,monitor:DP-2"
+      ] else [];
 
-        input = {
-            kb_layout = "gb";
-            follow_mouse = 1;
-            touchpad = {
-                natural_scroll = "yes";
-            };
+      input = {
+        kb_layout = "gb";
+        follow_mouse = 1;
+        touchpad = {
+          natural_scroll = "yes";
         };
-        #force_default_wallpaper = 0;
+      };
+      #force_default_wallpaper = 0;
 
-        general = {
-            gaps_in = 3;
-            gaps_out = 8;
-            border_size = 2;
+      general = {
+        gaps_in = 3;
+        gaps_out = 8;
+        border_size = 2;
+      };
+      decoration = {
+        rounding = 8;
+        blur = {
+          enabled = true;
+          size = 12;
+          passes = 4;
+          new_optimizations = true;
+          ignore_opacity = true;
+          xray = true;
+          popups = true;
         };
-        decoration = {
-            rounding = 8;
-            blur = {
-                enabled = true;
-                size = 12;
-                passes = 4;
-                new_optimizations = true;
-                ignore_opacity = true;
-                xray = true;
-                popups = true;
-            };
-            active_opacity = 0.95;
-            inactive_opacity = 0.9;
-            fullscreen_opacity = 1;
-            drop_shadow = true;
-            shadow_range = 30;
-            shadow_render_power = 3;
-        };
+        active_opacity = 0.95;
+        inactive_opacity = 0.9;
+        fullscreen_opacity = 1;
+        drop_shadow = true;
+        shadow_range = 30;
+        shadow_render_power = 3;
+      };
 
-        animations = {
-            enabled = 1;
-            animation = [
-                "windows,1,7,default"
-                "border,1,10,default"
-                "fade,1,10,default"
-                "workspaces,1,6,default"
-            ];
-        };
-
-        dwindle = {
-            pseudotile = 0; # enable pseudotiling on dwindle
-            preserve_split = true;
-        };
-
-        gestures = {
-            workspace_swipe = true;
-            workspace_swipe_touch = true;
-        };
-
-        misc = {
-          disable_splash_rendering = true;
-          disable_hyprland_logo = true;
-          enable_swallow = true;
-          swallow_regex = "^(kitty|alacritty)$";
-        };
-
-        # Autostart
-        exec-once = [
-            "waybar"
-            "mako"
-            #"hyprpaper"
-            "sleep 5 && emacs --daemon"
-            "ollama serve"
-            "sleep 10 && aw-qt"
-
-            "[workspace 1 silent] zen"
+      animations = {
+        enabled = 1;
+        animation = [
+          "windows,1,7,default"
+          "border,1,10,default"
+          "fade,1,10,default"
+          "workspaces,1,6,default"
         ];
-        # Window rules
-        windowrule = [
-            "monitor 0,discord"
-            "workspace 6,discord"
-            "monitor 0,WebCord"
-            "workspace 6,WebCord"
-            "monitor 0,whatsapp"
-            "workspace 6,whatsapp"
+      };
 
-            "float,com.usebottles.bottles"
+      dwindle = {
+        pseudotile = 0; # enable pseudotiling on dwindle
+        preserve_split = true;
+      };
+
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_touch = true;
+      };
+
+      misc = {
+        disable_splash_rendering = true;
+        disable_hyprland_logo = true;
+        enable_swallow = true;
+        swallow_regex = "^(kitty|alacritty)$";
+      };
+
+      # Autostart
+      exec-once = [
+        "waybar"
+        "mako"
+        #"hyprpaper"
+        "sleep 5 && emacs --daemon"
+        "ollama serve"
+        "sleep 10 && aw-qt"
+
+        "[workspace 1 silent] zen"
+      ];
+      # Window rules
+      windowrule = [
+        "monitor 0,discord"
+        "workspace 6,discord"
+        "monitor 0,WebCord"
+        "workspace 6,WebCord"
+        "monitor 0,whatsapp"
+        "workspace 6,whatsapp"
+
+        "float,com.usebottles.bottles"
+      ];
+      # Binds
+      bindm = [
+        "SUPER,mouse:272,movewindow"
+        "SUPER,mouse:273,resizewindow"
+      ];
+      bind = [
+        "SUPER,RETURN,exec,${pkgs.kitty}/bin/kitty"
+        "SUPER,Q,killactive"
+        "SUPER,L,exec,swaylock"
+        "SUPERSHIFT,L,exit"
+        "SUPER,space,togglefloating"
+        "SUPER,R,exec,fuzzel"
+        # Runs any package from the nix repository without installing.
+        "SUPERSHIFT,R,exec,${run-package}/bin/run-package"
+        "SUPERSHIFT,space,pseudo"
+        "SUPER,F,fullscreen"
+        "SUPER,N,exec,nautilus -w"
+        "SUPER,M,exec,zen"
+        "SUPER,E,exec,emacsclient --create-frame --alternate-editor=\"\" -a=\"\""
+        "SUPERSHIFT,E,exec,power-menu"
+        "SUPER,V,exec,mpv $(wl-paste) & notify-send \"Opening $(wl-paste) in mpv.\""
+        "SUPER,Y,exec,freetube $(wl-paste) & notify-send \"Opening $(wl-paste) in freetube.\""
+        "CTRL_SHIFT,escape,exec,${pkgs.mission-center}/bin/missioncenter"
+        "SUPER,X,exec,${pkgs.wl-kbptr}/bin/wl-kbptr"
+        
+        # Screenshots
+        ",Print,exec,grimblast --notify --cursor copy active"
+        "SHIFT,Print,exec,grimblast --notify --cursor copysave active"
+        "SUPER,S,exec,grimblast --notify --cursor copy area"
+        "SUPERSHIFT,S,exec,grimblast --notify --cursor copysave area"
+
+        "SUPER,left,movefocus,l"
+        "SUPER,right,movefocus,r"
+        "SUPER,up,movefocus,u"
+        "SUPER,down,movefocus,d"
+        "SUPERALT,n,movefocus,l"
+        "SUPERALT,i,movefocus,r"
+        "SUPERALT,u,movefocus,u"
+        "SUPERALT,e,movefocus,d"
+
+        "SUPERSHIFT,left,swapwindow,l"
+        "SUPERSHIFT,right,swapwindow,r"
+        "SUPERSHIFT,up,swapwindow,u"
+        "SUPERSHIFT,down,swapwindow,d"
+
+        "SUPER,1,workspace,1"
+        "SUPER,2,workspace,2"
+        "SUPER,3,workspace,3"
+        "SUPER,4,workspace,4"
+        "SUPER,5,workspace,5"
+        "SUPER,6,workspace,6"
+        "SUPER,7,workspace,7"
+        "SUPER,8,workspace,8"
+        "SUPER,9,workspace,9"
+        "SUPER,0,workspace,10"
+
+        "SUPERSHIFT,1,movetoworkspacesilent,1"
+        "SUPERSHIFT,2,movetoworkspacesilent,2"
+        "SUPERSHIFT,3,movetoworkspacesilent,3"
+        "SUPERSHIFT,4,movetoworkspacesilent,4"
+        "SUPERSHIFT,5,movetoworkspacesilent,5"
+        "SUPERSHIFT,6,movetoworkspacesilent,6"
+        "SUPERSHIFT,7,movetoworkspacesilent,7"
+        "SUPERSHIFT,8,movetoworkspacesilent,8"
+        "SUPERSHIFT,9,movetoworkspacesilent,9"
+        "SUPERSHIFT,0,movetoworkspacesilent,10"
+
+        "SUPER,mouse_down,workspace,e+1"
+        "SUPER,mouse_up,workspace,e-1"
+        "SUPER,f4,exec,hyprctl kill"
+      ];
+      bindl = [
+        ",switch:Lid,exec,swaylock"
+      ];
+      "plugin:touch" = {
+        sensitivity = "4.0";
+      };
+      "plugin:touch_gestures" = {
+        hyprgrass-bind = [
+          ", swipe:3:u, exec, fuzzel"
+          ", edge:r:l, workspace, +1"
+          ", longpress:3, resizewindow"
         ];
-        # Binds
-        bindm = [
-            "SUPER,mouse:272,movewindow"
-            "SUPER,mouse:273,resizewindow"
-        ];
-        bind = [
-            "SUPER,RETURN,exec,${pkgs.kitty}/bin/kitty"
-            "SUPER,Q,killactive"
-            "SUPER,L,exec,swaylock"
-            "SUPERSHIFT,L,exit"
-            "SUPER,space,togglefloating"
-            "SUPER,R,exec,fuzzel"
-            # Runs any package from the nix repository without installing.
-            "SUPERSHIFT,R,exec,${run-package}/bin/run-package"
-            "SUPERSHIFT,space,pseudo"
-            "SUPER,F,fullscreen"
-            "SUPER,N,exec,nautilus -w"
-            "SUPER,M,exec,zen"
-            "SUPER,E,exec,emacsclient --create-frame --alternate-editor=\"\" -a=\"\""
-            "SUPERSHIFT,E,exec,power-menu"
-            "SUPER,V,exec,mpv $(wl-paste) & notify-send \"Opening $(wl-paste) in mpv.\""
-            "SUPER,Y,exec,freetube $(wl-paste) & notify-send \"Opening $(wl-paste) in freetube.\""
-            "CTRL_SHIFT,escape,exec,${pkgs.mission-center}/bin/missioncenter"
-            "SUPER,X,exec,${pkgs.wl-kbptr}/bin/wl-kbptr"
-            
-            # Screenshots
-            ",Print,exec,grimblast --notify --cursor copy active"
-            "SHIFT,Print,exec,grimblast --notify --cursor copysave active"
-            "SUPER,S,exec,grimblast --notify --cursor copy area"
-            "SUPERSHIFT,S,exec,grimblast --notify --cursor copysave area"
-
-            "SUPER,left,movefocus,l"
-            "SUPER,right,movefocus,r"
-            "SUPER,up,movefocus,u"
-            "SUPER,down,movefocus,d"
-            "SUPERALT,n,movefocus,l"
-            "SUPERALT,i,movefocus,r"
-            "SUPERALT,u,movefocus,u"
-            "SUPERALT,e,movefocus,d"
-
-            "SUPERSHIFT,left,swapwindow,l"
-            "SUPERSHIFT,right,swapwindow,r"
-            "SUPERSHIFT,up,swapwindow,u"
-            "SUPERSHIFT,down,swapwindow,d"
-
-            "SUPER,1,workspace,1"
-            "SUPER,2,workspace,2"
-            "SUPER,3,workspace,3"
-            "SUPER,4,workspace,4"
-            "SUPER,5,workspace,5"
-            "SUPER,6,workspace,6"
-            "SUPER,7,workspace,7"
-            "SUPER,8,workspace,8"
-            "SUPER,9,workspace,9"
-            "SUPER,0,workspace,10"
-
-            "SUPERSHIFT,1,movetoworkspacesilent,1"
-            "SUPERSHIFT,2,movetoworkspacesilent,2"
-            "SUPERSHIFT,3,movetoworkspacesilent,3"
-            "SUPERSHIFT,4,movetoworkspacesilent,4"
-            "SUPERSHIFT,5,movetoworkspacesilent,5"
-            "SUPERSHIFT,6,movetoworkspacesilent,6"
-            "SUPERSHIFT,7,movetoworkspacesilent,7"
-            "SUPERSHIFT,8,movetoworkspacesilent,8"
-            "SUPERSHIFT,9,movetoworkspacesilent,9"
-            "SUPERSHIFT,0,movetoworkspacesilent,10"
-
-            "SUPER,mouse_down,workspace,e+1"
-            "SUPER,mouse_up,workspace,e-1"
-            "SUPER,f4,exec,hyprctl kill"
-        ];
-        bindl = [
-          ",switch:on:lid,exec,swaylock"
-        ];
-        "plugin:touch" = {
-          sensitivity = "4.0";
-        };
-        "plugin:touch_gestures" = {
-          hyprgrass-bind = [
-            ", swipe:3:u, exec, fuzzel"
-            ", edge:r:l, workspace, +1"
-            ", longpress:3, resizewindow"
-          ];
-        };
+      };
     };
     extraConfig = ''
         # Fix Steam
