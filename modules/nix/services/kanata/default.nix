b6f291d3ad98ddcf903559df3a392889a7e78c62
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2024-10-27 14:14:39 +0000

    swap caps and esc

--- modules/nix/services/kanata/default.nix
+++ modules/nix/services/kanata/default.nix
@@ -7,13 +7,15 @@
       "surface" = {
         config = ''
           (defsrc
+            esc
             q w e r t y u i o p
-            a s d f g h j k l ;
+            caps a s d f g h j k l ;
             z x c v b n m
           )
           (deflayer colemak-dht
+            caps
             q w f p b j l u y ;
-            a r s t g k n e i o
+            esc a r s t g k n e i o
             x c d v z m h
           )
         '';
