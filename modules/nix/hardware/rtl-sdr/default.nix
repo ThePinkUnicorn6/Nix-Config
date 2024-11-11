56967b1644fa93713a30be36c866bc5f278a8f49
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2024-11-11 21:23:04 +0000

    432 current  2024-11-11 21:23:04  24.11.20241105.4aa3656  6.6.59                          *

--- modules/nix/hardware/rtl-sdr/default.nix
+++ modules/nix/hardware/rtl-sdr/default.nix
@@ -1,5 +1,9 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, settings, ... }:
 
 {
   hardware.rtl-sdr.enable = true;
+  environment.systemPackages = with pkgs; [
+    rtl-sdr
+  ];
+  users.users.${settings.username}.extraGroups = [ "plugdev" ];
 }
