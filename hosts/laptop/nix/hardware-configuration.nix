4d1638f6e112c36a596f4b8ff3f8a6c2b5e24c14
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2024-12-02 13:22:54 +0000

    Update laptop hardware config

--- hosts/laptop/nix/hardware-configuration.nix
+++ hosts/laptop/nix/hardware-configuration.nix
@@ -26,15 +26,14 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/26e57e2c-ba3e-46b2-a9ad-86a92b4d13b7"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
