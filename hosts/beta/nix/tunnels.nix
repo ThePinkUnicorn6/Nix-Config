68271b8f0a286416dd3e88c20c9bf0da97c34dee
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2024-12-23 18:53:03 +0000

    Switch to autossh

--- hosts/beta/nix/tunnels.nix
+++ hosts/beta/nix/tunnels.nix
@@ -1,14 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-  systemd.services."immich-tunnel-to-dietpi" = {
-    enable = true;
-    requires = [ "tailscaled.service" "network-online.target" ];
-    after = [ "sys-subsystem-net-devices-tailscale0.device" "tailscaled.service" "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''${pkgs.openssh}/bin/ssh -N -R 0.0.0.0:80:localhost:3002 dietpi@100.119.57.79'';
-    };
-  };
+  services.autossh.sessions = [
+    {
+      name = "immich-dietpi-tunnel";
+      user = "beta";
+      extraArguments = "-N -R 0.0.0.0:80:localhost:3002 dietpi@dietpi";
+    }
+  ];
 }
