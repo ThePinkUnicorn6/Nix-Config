b283aa6754303bf2745f1b5e602e398b304d9eae
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2025-01-05 16:12:29 +0000

    456 current  2025-01-05 16:12:29  25.05.20241217.d3c42f1  6.6.66                          *

--- modules/nix/hardware/openrgb/default.nix
+++ modules/nix/hardware/openrgb/default.nix
@@ -15,43 +15,43 @@
         '';
       }));
       
-      # openrgb-plugin-hardwaresync =  (prev.openrgb-plugin-hardwaresync.overrideAttrs (oldAttrs: {
-      #   src = pkgs.fetchFromGitLab {
-      #     owner = "OpenRGBDevelopers";
-      #     repo = "OpenRGBHardwareSyncPlugin";
-      #     rev = "bbd365f974e423dfc2d3e3adabffdcc027ac0959";
-      #     hash = "sha256-De0ARYw06yNMAt+QrWEe1jCg0AfAw7sX68L276pZXfA=";
-      #     fetchSubmodules = true;
-      #   };
-      #   postPatch = ''
-      #     # Use the source of openrgb from nixpkgs instead of the submodule
-      #     rm -r OpenRGB
-      #     ln -s ${final.openrgb.src} OpenRGB
-      #     # Remove prebuilt stuff
-      #     rm -r dependencies/lhwm-cpp-wrapper
-      #   '';
-      # }));
+      openrgb-plugin-hardwaresync =  (prev.openrgb-plugin-hardwaresync.overrideAttrs (oldAttrs: {
+        src = pkgs.fetchFromGitLab {
+          owner = "OpenRGBDevelopers";
+          repo = "OpenRGBHardwareSyncPlugin";
+          rev = "bbd365f974e423dfc2d3e3adabffdcc027ac0959";
+          hash = "sha256-De0ARYw06yNMAt+QrWEe1jCg0AfAw7sX68L276pZXfA=";
+          fetchSubmodules = true;
+        };
+        postPatch = ''
+          # Use the source of openrgb from nixpkgs instead of the submodule
+          rm -r OpenRGB
+          ln -s ${final.openrgb.src} OpenRGB
+          # Remove prebuilt stuff
+          rm -r dependencies/lhwm-cpp-wrapper
+        '';
+      }));
       
-      # openrgb-plugin-effects =  (prev.openrgb-plugin-effects.overrideAttrs (oldAttrs: {
-      #   src = pkgs.fetchFromGitLab {
-      #     owner = "OpenRGBDevelopers";
-      #     repo = "OpenRGBEffectsPlugin";
-      #     rev = "f05a38bb";
-      #     hash = "sha256-FCrtY7XcbhQPBMBn02yJ5G/5UJVzlfF+Xa/xSonbHEo=";
-      #     fetchSubmodules = true;
-      #   };
-      # }));
+      openrgb-plugin-effects =  (prev.openrgb-plugin-effects.overrideAttrs (oldAttrs: {
+        src = pkgs.fetchFromGitLab {
+          owner = "OpenRGBDevelopers";
+          repo = "OpenRGBEffectsPlugin";
+          rev = "f05a38bb";
+          hash = "sha256-FCrtY7XcbhQPBMBn02yJ5G/5UJVzlfF+Xa/xSonbHEo=";
+          fetchSubmodules = true;
+        };
+      }));
     })
   ];
   services.hardware.openrgb = {
     enable = true;
-    package = (pkgs.openrgb-new.withPlugins [
+    package = (pkgs.openrgb.withPlugins [
       pkgs.openrgb-plugin-hardwaresync
       pkgs.openrgb-plugin-effects
     ]);
   };
   environment.systemPackages = with pkgs; [
-    (pkgs.openrgb-new.withPlugins [
+    (pkgs.openrgb.withPlugins [
       pkgs.openrgb-plugin-hardwaresync
       pkgs.openrgb-plugin-effects
     ])
