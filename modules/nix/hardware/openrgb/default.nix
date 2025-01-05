0501db4495cfb196a511bbe7d63705eac2533bfc
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2025-01-05 18:44:32 +0000

    Finally got nzxt fans working with openrgb

--- modules/nix/hardware/openrgb/default.nix
+++ modules/nix/hardware/openrgb/default.nix
@@ -1,59 +1,17 @@
 { config, lib, pkgs, ... }:
 {
-  nixpkgs.overlays = [
+  nixpkgs.overlays = with pkgs; [
     (final: prev: {
       openrgb = (prev.openrgb.overrideAttrs (oldAttrs: {
-        src = pkgs.fetchFromGitLab {
-          owner = "CalcProgrammer1";
-          repo = "OpenRGB";
-          rev = "c4917d30b027becd2d9f639deb975006e58b59ba";
-          hash = "sha256-V9pwdiRkeaX8KPzilbgc1cGLukzatcvtuAkpjSlhdwA=";
-        };
-        postPatch = oldAttrs.postPatch + ''
-          substituteInPlace scripts/build-udev-rules.sh \
-            --replace "/usr/bin/env chmod" "${pkgs.coreutils}/bin/chmod"
-        '';
-      }));
-      
-      openrgb-plugin-hardwaresync =  (prev.openrgb-plugin-hardwaresync.overrideAttrs (oldAttrs: {
-        src = pkgs.fetchFromGitLab {
-          owner = "OpenRGBDevelopers";
-          repo = "OpenRGBHardwareSyncPlugin";
-          rev = "bbd365f974e423dfc2d3e3adabffdcc027ac0959";
-          hash = "sha256-De0ARYw06yNMAt+QrWEe1jCg0AfAw7sX68L276pZXfA=";
-          fetchSubmodules = true;
-        };
-        postPatch = ''
-          # Use the source of openrgb from nixpkgs instead of the submodule
-          rm -r OpenRGB
-          ln -s ${final.openrgb.src} OpenRGB
-          # Remove prebuilt stuff
-          rm -r dependencies/lhwm-cpp-wrapper
-        '';
-      }));
-      
-      openrgb-plugin-effects =  (prev.openrgb-plugin-effects.overrideAttrs (oldAttrs: {
-        src = pkgs.fetchFromGitLab {
-          owner = "OpenRGBDevelopers";
-          repo = "OpenRGBEffectsPlugin";
-          rev = "f05a38bb";
-          hash = "sha256-FCrtY7XcbhQPBMBn02yJ5G/5UJVzlfF+Xa/xSonbHEo=";
-          fetchSubmodules = true;
-        };
+        patches = [ ./nzxt_f120_core_fan.patch ];
       }));
     })
   ];
   services.hardware.openrgb = {
     enable = true;
-    package = (pkgs.openrgb.withPlugins [
-      pkgs.openrgb-plugin-hardwaresync
-      pkgs.openrgb-plugin-effects
-    ]);
+    package = pkgs.openrgb-with-all-plugins;
   };
   environment.systemPackages = with pkgs; [
-    (pkgs.openrgb.withPlugins [
-      pkgs.openrgb-plugin-hardwaresync
-      pkgs.openrgb-plugin-effects
-    ])
+    openrgb-with-all-plugins
   ];
 }
