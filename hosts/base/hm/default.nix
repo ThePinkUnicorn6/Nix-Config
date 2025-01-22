b76760454942b36be5cc856a6454fb285d342540
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2025-01-22 22:41:20 +0000

    Make hm gc

--- hosts/base/hm/default.nix
+++ hosts/base/hm/default.nix
@@ -1,5 +1,9 @@
 { lib, config, pkgs, settings, ... }:
 
 {
-
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 7d"; # delete old generations
+    frequency = "daily";
+  };
 }
