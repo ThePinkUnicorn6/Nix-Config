0e89cfd7c9ab1e83c3bd712c5b355419ef32a72a
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2025-07-30 21:17:28 +0100

    Add steamlibrary on external drive

--- hosts/desktop/nix/default.nix
+++ hosts/desktop/nix/default.nix
@@ -49,6 +49,16 @@
     device = "/dev/nvme0n1p4";
     fsType = "ntfs";
   };
+  fileSystems."/home/${settings.username}/steamLibrary" = {
+    device = "/dev/disk/by-uuid/eccf07d2-930d-48bf-b97e-d349232c2f04";
+    fsType = "ext4";
+    options = [ # If you don't have this options attribute, it'll default to "defaults" 
+     # boot options for fstab. Search up fstab mount options you can use
+     "users" # Allows any user to mount and unmount
+     "nofail" # Prevent system from failing if this drive doesn't mount
+     "exec" # Permit execution of binaries and other executable files
+   ];
+  };
   environment.systemPackages = with pkgs; [
     protonup-qt # For cutstom proton versions
   ];
