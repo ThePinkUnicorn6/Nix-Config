1565b1201bbae840af632ee8099cf40f2301a672
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2025-11-07 03:12:46 +0000

    Add tlp

--- hosts/laptop/nix/default.nix
+++ hosts/laptop/nix/default.nix
@@ -10,6 +10,7 @@
     "/style"
     "/app/fido2"
     "/services/kanata"
+    /services/tlp
   ]);
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -18,27 +19,18 @@
     description = settings.name;
     extraGroups = [ "networkmanager" "wheel" "adbusers" "dialout" "audio" "camera" "uinput" ];
   };
-#  boot.blacklistedKernelModules = [ 
-#    "ipu3_imgu"
-#  ]; 
 
   networking = {
     hostName = "laptop"; # Define your hostname.
   };
   nix.settings.trusted-users = [ "root" settings.username ];
 
-#  fileSystems."/home/${settings.username}/shared-files" = {
-#    device = "/dev/nvme0n1p4";
-#    fsType = "ntfs";
-#  };
-  
   # Configure keymap in X11
   services = {
     xserver.xkb = {
       layout = "gb";
       variant = "";
     };
-#    iptsd.config.Touchscreen.DisableOnPalm = true;
     
     flatpak.enable = true;
     openssh.enable = true;
