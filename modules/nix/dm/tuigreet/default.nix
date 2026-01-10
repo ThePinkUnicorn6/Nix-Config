c0547b460122cc20ccef43129618aacb97daa0f5
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2026-01-10 17:40:15 +0000

    fix tuigreet login with sway

--- modules/nix/dm/tuigreet/default.nix
+++ modules/nix/dm/tuigreet/default.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --command 'dbus-run-session say' --remember --asterisks";
         user = "greeter";
       };
     };
